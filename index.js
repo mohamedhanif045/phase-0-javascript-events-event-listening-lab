fca6d7b80139a2e0cb81c1cabfeff48b4fbd0e86
mohamedhanif045 <hanifmohamed045@gmail.com>
2024-05-28 13:09:50 +0300

    event-listener

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+
+// index.js
 function addingEventListener() {
-}
+    const input = document.getElementById('button');
+  
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+  
+    input.addEventListener('click', clickAlert);
+  }
+  
+  // Call the function to set up the event listener
+  addingEventListener();
+  
